0585dcc48d70866df3a50d713531ca20f4d020df
charakdivij-cpu <charakdivij@gmail.com>
2025-11-23 00:02:34 +0530

    27th commit

---
+++ .vscode/creating function.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Function to check prime
+int isPrime(int n) {
+    int i;
+
+    if (n <= 1)
+        return 0; // not prime
+
+    for (i = 2; i <= n / 2; i++) {
+        if (n % i == 0)
+            return 0; // not prime
+    }
+
+    return 1; // prime
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPrime(num))
+        printf("%d is a prime number.", num);
+    else
+        printf("%d is
