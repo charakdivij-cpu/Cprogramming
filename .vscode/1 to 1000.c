f7f3eda464dee25a215c33d8133dad893696f484
charakdivij-cpu <charakdivij@gmail.com>
2025-11-22 23:45:34 +0530

    22th commit

---
+++ .vscode/1 to 1000.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num, rem, sum, original;
+
+    printf("Armstrong numbers between 1 and 1000 are:\n");
+
+    for (num = 1; num <= 1000; num++) {
+        sum = 0;
+        original = num;
+
+        while (orig
