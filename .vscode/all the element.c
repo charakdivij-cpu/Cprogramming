a297953ba58d6bd8e4c8459261c76e360c20cd74
charakdivij-cpu <charakdivij@gmail.com>
2025-11-23 10:04:57 +0530

    39th commit

---
+++ .vscode/all the element.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr1[n], arr2[n];
+
+    printf("Enter elements of first array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &ar
